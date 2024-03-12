a3f1a8acca7218ea56ec32977038166f0aff2cf3
Chanju03 <129511216+Chanju03@users.noreply.github.com>
2024-03-13 02:36:28 +0900

    재확인

--- src/page/MyPage/GuideMyPage.tsx
+++ src/page/MyPage/GuideMyPage.tsx
@@ -11,6 +11,21 @@ function GuideMyPage() {
   const [password, setPassword] = useState("");
   const [phoneNum, setPhoneNum] = useState("");
   const [registeredAt, setRegisteredAt] = useState("");
+  const [EmailFixPopUp, setEmailFixPopUp] = useState(false);
+  const [isBlurred, seIsBlurred] = useState(true);
+  const [PhoneNumFixPopUp, setPhoneNumFixPopUp] = useState(false);
+  const [PhonenNumtarget, setPhonenNumtarget] = useState<string>("");
+  const [PhoneNumRegisterPassword, setPhoneNumRegisterPassword] =
+    useState<string>("");
+  const [PhoneNumFixPopUpError, setPhoneNumFixPopUpError] = useState(false);
+  const [PasswordChange, setPasswordChange] = useState<string>("");
+  const [NewPasswordChange, setNewPasswordChange] = useState<string>("");
+  const [PasswordFixPopUp, setPasswordFixPopUp] = useState(false);
+  const [PasswordFixPopUpError, setPasswordFixPopUpError] = useState(false);
+  const [target, setTarget] = useState<string>("");
+  const [NameRegisterPassword, setNameRegisterPassword] = useState("");
+  const [NameFixPopUp, setNameFixPopUp] = useState(false);
+  const [NameFixPopUpError, setNameFixPopUpError] = useState(false);
 
   useEffect(() => {
     if (token === null) {
@@ -40,10 +55,137 @@ function GuideMyPage() {
 
     GuideMyInformation();
   }, [token]);
+  // 이메일 수정, 등록
+  const EmailFixClick = () => {
+    setEmailFixPopUp(true);
+    seIsBlurred(false);
+  };
+  const EmailFixCancelClick = () => {
+    setEmailFixPopUp(false);
+    seIsBlurred(true);
+  };
+
+  // 비밀번호 변경
+
+  interface ChangePasswordData {
+    password: string;
+    newPassword: string;
+  }
+
+  const ChangePasswordRegisterOnClick = async () => {
+    const data: ChangePasswordData = {
+      password: PasswordChange,
+      newPassword: NewPasswordChange,
+    };
+    try {
+      const response = await axios.post("/v1/profile/password", data, {
+        headers: {
+          "X-AUTH-TOKEN": token,
+        },
+      });
+
+      if (response.status === 200) {
+        console.log("비밀번호 재설정 성공", response.data);
+        window.location.reload();
+      }
+    } catch (error) {
+      console.error("비밀번호 재설정 실패:", error);
+      setPasswordFixPopUpError(true);
+    }
+  };
+
+  const PasswordFixClick = () => {
+    setPasswordFixPopUp(true);
+    seIsBlurred(false);
+  };
+  const PasswordFixCancelClick = () => {
+    setPasswordFixPopUp(false);
+    seIsBlurred(true);
+    setPasswordFixPopUpError(false);
+  };
+
+  // 이름 등록, 변경
+
+  interface NameRegisterData {
+    target: string;
+    password: string;
+  }
+
+  const NameRegisterOnClick = async () => {
+    const data: NameRegisterData = {
+      target: target,
+      password: NameRegisterPassword,
+    };
+
+    try {
+      const response = await axios.post("/v1/profile/name", data, {
+        headers: {
+          "X-AUTH-TOKEN": token,
+        },
+      });
+
+      if (response.status === 200) {
+        console.log("실명등록 성공", response.data);
+        window.location.reload();
+      }
+    } catch (error) {
+      console.error("실명등록 실패:", error);
+      setNameFixPopUpError(true);
+    }
+  };
+
+  const NameFixClick = () => {
+    setNameFixPopUp(true);
+    seIsBlurred(false);
+  };
+  const NameFixCancelClick = () => {
+    setNameFixPopUp(false);
+    seIsBlurred(true);
+    setNameFixPopUpError(false);
+  };
+
+  // 전화 번호 수정, 등록
+
+  interface PhoneNumRegisterData {
+    target: string;
+    password: string;
+  }
+
+  const PhoneNumRegisterOnClick = async () => {
+    const data: PhoneNumRegisterData = {
+      target: PhonenNumtarget,
+      password: PhoneNumRegisterPassword,
+    };
+    try {
+      const response = await axios.post("/v1/profile/phone", data, {
+        headers: {
+          "X-AUTH-TOKEN": token,
+        },
+      });
+
+      if (response.status === 200) {
+        console.log("전화등록 성공", response.data);
+        window.location.reload();
+      }
+    } catch (error) {
+      console.error("전화등록 실패:", error);
+      setPhoneNumFixPopUpError(true);
+    }
+  };
+
+  const PhoneNumFixClick = () => {
+    setPhoneNumFixPopUp(true);
+    seIsBlurred(false);
+  };
+  const PhoneNumFixCancelClick = () => {
+    setPhoneNumFixPopUp(false);
+    seIsBlurred(true);
+    setPhoneNumFixPopUpError(false);
+  };
   return (
     <div className="GuideMyPageFrame">
       <HeaderTwo></HeaderTwo>
-      <div className="GuideMyPageInner">
+      <div className={isBlurred ? "GuideMyPageInner" : "GuideMyPageInnerBlur"}>
         <div className="GuideMyPageInnerTextOne">내 정보</div>
         <div className="GuideMyPageInfoContainer">
           <div className="GuideMyPageInfoBoxline"></div>
@@ -51,7 +193,12 @@ function GuideMyPage() {
             <div className="GuideMyPageInfoBoxOneTextOne">이메일 주소</div>
             <div className="GuideMyPageInfoBoxOneInner">
               <div className="GuideMyPageInfoText">{email}</div>
-              <div className="GuideMyPageInfoBoxButtonOne">수정</div>
+              <div
+                className="GuideMyPageInfoBoxButtonOne"
+                onClick={EmailFixClick}
+              >
+                수정
+              </div>
             </div>
           </div>
           <div className="GuideMyPageInfoBoxline"></div>
@@ -59,7 +206,12 @@ function GuideMyPage() {
             <div className="GuideMyPageInfoBoxOneTextOne">비밀번호</div>
             <div className="GuideMyPageInfoBoxOneInner">
               <div className="GuideMyPageInfoText">{password}</div>
-              <div className="GuideMyPageInfoBoxButtonOne">변경</div>
+              <div
+                className="GuideMyPageInfoBoxButtonOne"
+                onClick={PasswordFixClick}
+              >
+                변경
+              </div>
             </div>
           </div>
           <div className="GuideMyPageInfoBoxline"></div>
@@ -67,7 +219,12 @@ function GuideMyPage() {
             <div className="GuideMyPageInfoBoxOneTextOne">이름</div>
             <div className="GuideMyPageInfoBoxOneInner">
               <div className="GuideMyPageInfoText">{name}</div>
-              <div className="GuideMyPageInfoBoxButtonOne">수정</div>
+              <div
+                className="GuideMyPageInfoBoxButtonOne"
+                onClick={NameFixClick}
+              >
+                수정
+              </div>
             </div>
           </div>
           <div className="GuideMyPageInfoBoxline"></div>
@@ -75,7 +232,12 @@ function GuideMyPage() {
             <div className="GuideMyPageInfoBoxOneTextOne">전화번호</div>
             <div className="GuideMyPageInfoBoxOneInner">
               <div className="GuideMyPageInfoText">{phoneNum}</div>
-              <div className="GuideMyPageInfoBoxButtonOne">수정</div>
+              <div
+                className="GuideMyPageInfoBoxButtonOne"
+                onClick={PhoneNumFixClick}
+              >
+                수정
+              </div>
             </div>
           </div>
           <div className="GuideMyPageInfoBoxline"></div>
@@ -93,6 +255,8 @@ function GuideMyPage() {
             </div>
           </div>
         </div>
+      </div>
+      {EmailFixPopUp && (
         <div className="EmailFixPopUpFrame">
           <div className="EmailFixPopUpInner">
             <div className="EmailFixPopUpTextOne">새로운 이메일 주소 등록</div>
@@ -129,12 +293,177 @@ function GuideMyPage() {
               </div>
             </div>
             <div className="EmailFixPopUpButtonContainer">
-              <div className="EmailFixPopUpCancleButton">취소</div>
+              <div
+                className="EmailFixPopUpCancleButton"
+                onClick={EmailFixCancelClick}
+              >
+                취소
+              </div>
               <div className="EmailFixPopUpRegisterButton">등록</div>
             </div>
           </div>
         </div>
-      </div>
+      )}
+      {PhoneNumFixPopUp && (
+        <div className="FixConponentFrame">
+          <div className="FixConponentInner">
+            <div className="FixConponentTitle">전화번호 등록 및 변경</div>
+            {PhoneNumFixPopUpError && (
+              <div className="EmailFixPopUpAlert">
+                <img
+                  className="EmailFixPopUpAlertImg"
+                  src="../img/EmailFixPopUpAlertImg.svg"
+                  alt="!"
+                ></img>
+                <div className="EmailFixPopUpAlertText">
+                  비밀번호를 다시 확인해 주세요
+                </div>
+              </div>
+            )}
+            <div className="FixConponentInputFrame">
+              <div className="FixConponentInputInner">
+                <div className="FixConponentInputTitle">전화번호</div>
+                <div className="FixConponentInputTitle">비밀번호</div>
+              </div>
+              <div className="FixConponentInputSecondInner">
+                <input
+                  placeholder="010-XXXX-XXXX 형식으로 입력"
+                  className="FixConponentFirstInput"
+                  onChange={(e) => setPhonenNumtarget(e.target.value)}
+                ></input>
+                <input
+                  className="FixConponentSecondInput"
+                  placeholder="현재 비밀번호"
+                  type="password"
+                  onChange={(e) => setPhoneNumRegisterPassword(e.target.value)}
+                ></input>
+              </div>
+            </div>
+            <div className="FixConponentButtonFrame">
+              <div
+                className="FixConponentCancleButton"
+                onClick={PhoneNumFixCancelClick}
+              >
+                취소
+              </div>
+              <div
+                className="FixConponentRegisterButton"
+                onClick={PhoneNumRegisterOnClick}
+              >
+                등록
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      {PasswordFixPopUp && (
+        <div className="ChangePasswordFrame">
+          <div className="ChangePasswordInner">
+            <div className="TextChangePassword">비밀번호 변경</div>
+            {PasswordFixPopUpError && (
+              <div className="EmailFixPopUpAlert">
+                <img
+                  className="EmailFixPopUpAlertImg"
+                  src="../img/EmailFixPopUpAlertImg.svg"
+                  alt="!"
+                ></img>
+                <div className="EmailFixPopUpAlertText">
+                  현재 비밀번호를 확인해 주세요 (8자리 이상, 특수문자, 대문자,
+                  숫자 1개 이상 포함)
+                </div>
+              </div>
+            )}
+            <div className="TextLastPassword">현재 비밀번호</div>
+            <input
+              className="LastPasswordInput"
+              type="password"
+              onChange={(e) => setPasswordChange(e.target.value)}
+            ></input>
+            <div className="NewPasswordTextFrame">
+              <div className="NewPasswordText">새 비밀번호</div>
+              <div className="NewPasswordText">새 비밀번호 재입력</div>
+            </div>
+            <div className="NewPasswordInputFrame">
+              <input
+                className="NewPasswordInput"
+                type="password"
+                onChange={(e) => setNewPasswordChange(e.target.value)}
+              ></input>
+              <input
+                className="NewPasswordInput"
+                type="password"
+                onChange={(e) => setNewPasswordChange(e.target.value)}
+              ></input>
+            </div>
+            <div className="ChangePasswordButtonFrame">
+              <div
+                className="ChangePasswordCancle"
+                onClick={PasswordFixCancelClick}
+              >
+                취소
+              </div>
+              <div
+                className="ChangePasswordRegister"
+                onClick={ChangePasswordRegisterOnClick}
+              >
+                변경
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      {NameFixPopUp && (
+        <div className="FixConponentFrame">
+          <div className="FixConponentInner">
+            <div className="FixConponentTitle">이름 등록 및 변경</div>
+            {NameFixPopUpError && (
+              <div className="EmailFixPopUpAlert">
+                <img
+                  className="EmailFixPopUpAlertImg"
+                  src="../img/EmailFixPopUpAlertImg.svg"
+                  alt="!"
+                ></img>
+                <div className="EmailFixPopUpAlertText">
+                  현재 비밀번호를 확인해 주세요
+                </div>
+              </div>
+            )}
+            <div className="FixConponentInputFrame">
+              <div className="FixConponentInputInner">
+                <div className="FixConponentInputTitle">이름</div>
+                <div className="FixConponentInputTitle">비밀번호</div>
+              </div>
+              <div className="FixConponentInputSecondInner">
+                <input
+                  placeholder="이름"
+                  className="FixConponentFirstInput"
+                  onChange={(e) => setTarget(e.target.value)}
+                ></input>
+                <input
+                  className="FixConponentSecondInput"
+                  placeholder="현재 비밀번호"
+                  type="password"
+                  onChange={(e) => setNameRegisterPassword(e.target.value)}
+                ></input>
+              </div>
+            </div>
+            <div className="FixConponentButtonFrame">
+              <div
+                className="FixConponentCancleButton"
+                onClick={NameFixCancelClick}
+              >
+                취소
+              </div>
+              <div
+                className="FixConponentRegisterButton"
+                onClick={NameRegisterOnClick}
+              >
+                등록
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
