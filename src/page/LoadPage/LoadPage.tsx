e3cbafca5d5dfd71b4e8fd557f7cb30fd9fcfe9f
Yun-Hwan Jeon <yunsol267@gmail.com>
2024-04-23 01:58:11 +0900

    Fix: 로딩 페이지 경로 수정

--- src/page/LoadPage/LoadPage.tsx
+++ src/page/LoadPage/LoadPage.tsx
@@ -7,7 +7,7 @@ function LoadPage() {
     <div className="LoadPageFrame">
       <HeaderTwo></HeaderTwo>
       <div className="LoadPageInner">
-        <img src="../img/loadImg.svg" alt="오류" className="loadImg"></img>
+        <img src="/img/loadImg.svg" className="loadImg"></img>
         <div className="LoadPageTitle">잠시만 기다려 주세요!</div>
         <div className="LoadPageDescription">
           사용자님의 정보를 불러오는 중이에요. <br />이 화면이 계속된다면
